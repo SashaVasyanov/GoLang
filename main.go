2cc5e074ef412e24a8cad8d12f67afc26b3e4787
Sasha Vasyanov <primarh.alfary@yandex.ru>
2021-09-02 23:04:14 +0300

    Learn go

---
+++ main.go
@@ -0,0 +1,39 @@
+// package main
+
+// import "fmt"
+
+/* ANCHOR ЛОГИЧЕСКИЕ (БУЛЕВЫЕ ТИПЫ) в go
+func main() {
+    fmt.Println(true && true) --- true
+    fmt.Println(true && false) --- false 
+    fmt.Println(true || true) --- true
+    fmt.Println(true || false) --- true
+    fmt.Println(!true) --- false 
+}
+*/
+
+// package main
+
+// import "fmt"
+
+// func main() {
+//     var xzx string 
+// 	xzx = "i went"
+// 	xzx += " to meeting last night"
+//     fmt.Println(xzx)
+// }
+
+
+// package main
+
+// import "fmt"
+
+// func main() {
+//     fmt.Print("Enter a number: ")
+//     var input float64
+//     fmt.Scanf("%f", &input)
+
+//     output := input * 2
+
+//     fmt.Println(output)    
+// }
